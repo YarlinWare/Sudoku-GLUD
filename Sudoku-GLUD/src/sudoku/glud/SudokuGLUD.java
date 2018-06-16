bb62766856c10f6f38bb41ecb85560e457be5743
YarlinWare <Cmyarap@correo.udistrital.edu.co>
2018-06-15 23:24:42 -0500

    Proyecto desarrollado en Java
    Proyecto base en desarrollo.

---
+++ Sudoku-GLUD/src/sudoku/glud/SudokuGLUD.java
@@ -0,0 +1,22 @@
+package sudoku.glud;
+
+public class SudokuGLUD {
+    
+    //Metodo principal
+    public static void main(String[] args) {
+        /*Interfaz_Usuario interfaz_user = new Interfaz_Usuario();        
+        interfaz_user.setVisible(true);          */
+        System.out.print("Hola mundo");
+        System.out.print("cual es tu nombre");
+        //new Interfaz_Usuario().setVisible(true);
+        
+       /* java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                new Interfaz_Usuario().setVisible(true);
+            }
+        });*/
+    }
+    
+
+}
